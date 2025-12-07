97a97d3079586720915de5189cfd28ea50219429
Nicolas Leroux <hertford.fr@gmail.com>
2025-12-07 11:36:13 +0100

    feat: add user

---
+++ src/main/java/eu/nicosworld/falloutback/infrastructure/listener/AuthenticatedUserCreator.java
@@ -0,0 +1,25 @@
+package eu.nicosworld.falloutback.infrastructure.listener;
+
+import eu.nicosworld.falloutback.authentication.model.User;
+import eu.nicosworld.falloutback.infrastructure.event.UserCreatedEvent;
+import eu.nicosworld.falloutback.infrastructure.persistence.entity.AuthenticatedUser;
+import eu.nicosworld.falloutback.infrastructure.persistence.repository.AuthenticatedUserRepository;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticatedUserCreator {
+
+    private final AuthenticatedUserRepository authenticatedUserRepository;
+
+    public AuthenticatedUserCreator(AuthenticatedUserRepository authenticatedUserRepository) {
+        this.authenticatedUserRepository = authenticatedUserRepository;
+    }
+
+    @EventListener
+    public void handleUserCreated(UserCreatedEvent event) {
+        User user = event.getUser();
+        AuthenticatedUser authenticatedUser = new AuthenticatedUser(user);
+        authenticatedUserRepository.save(authenticatedUser);
+    }
+}
